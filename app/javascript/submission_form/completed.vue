46e92653af0b85a8d1d76555e5e9b430a7637629
XIAOJING <xjg332767492@gmail.com>
2025-10-09 23:51:08 +0800

    release v1.0.5

--- app/javascript/submission_form/completed.vue
+++ app/javascript/submission_form/completed.vue
@@ -63,27 +63,7 @@
           {{ t('download') }}
         </span>
       </button>
-      <a
-        v-if="isDemo"
-        target="_blank"
-        href="https://github.com/docusealco/docuseal"
-        class="white-button flex items-center space-x-1 w-full"
-      >
-        <IconBrandGithub />
-        <span>
-          Star on Github
-        </span>
-      </a>
-      <a
-        v-if="isDemo"
-        href="https://docuseal.com/sign_up"
-        class="white-button flex items-center space-x-1 w-full"
-      >
-        <IconLogin />
-        <span>
-          {{ t('create_a_free_account') }}
-        </span>
-      </a>
+      
     </div>
     <div v-if="false" class="text-center mt-4"></div>
   </div>
